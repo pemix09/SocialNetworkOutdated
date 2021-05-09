db151cccc5c57b06669902950e1de1efb3167875
vistr593 <cezaryh1106@gmail.com>
2021-05-09 20:53:19 +0200

    models, klasa users

---
+++ Models/users.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Models
+{
+    public class users
+    {
+        //Wyjdzie kiedyś jakieś wyrażenia regularne wykorzystać
+        public int id { get; set; }
+        [Display(Name = "Imię")]
+        [MinLength(2)]
+        public string firstName { get; set; }
+        [Display(Name = "Nazwisko")]
+        [MinLength(2)]
+        public string lastName { get; set; }
+        [Display(Name = "Data urodzenia")]
+        [DataType(DataType.Date)]
+        public string birthDate { get; set; }
+        [Display(Name = "Nazwa użytkownika")]
+        [Required(ErrorMessage = "Pole 'Nazwa użytkownika' jest wymagane")]
+        public string nickname { get; set; }
+        [Display(Name =  "E-mail")]
+        [Required(ErrorMessage = "Pole 'E-mail' jest wymagane")]
+        public string email { get; set; }
+        [Display(Name =  "Nr telefonu")]
+        [MinLength(7)]
+        public Int64 phone { get; set; }
+    }
+}
