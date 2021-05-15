03f5a0e970c9c3258c61407129a8c17817ab1057
pemix09 <pemix09@onet.pl>
2021-05-15 16:55:58 +0200

    Dodałem stronę użytkownika, która na razie jest pusta

--- Pages/UserPage.cshtml.cs
+++ Pages/UserPage.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SocialNetwork.Pages
 {
-    public class ProfileSiteModel : PageModel
+    public class UserPageModel : PageModel
     {
         public void OnGet()
         {
