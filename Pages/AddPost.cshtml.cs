72381efbaf80f5405d5c66d7c2f6c3310d343421
Przemysław Klejno <pemix09@onet.pl>
2021-05-31 10:44:39 +0200

    Dodałem zajebusty ekran rejestracji

--- Pages/AddPost.cshtml.cs
+++ Pages/AddPost.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,6 +11,7 @@ using SocialNetwork.Models;
 
 namespace SocialNetwork.Pages
 {
+    [Authorize]
     public class AddPostModel : PageModel
     {
         [BindProperty]
