999b972e37c48216c39a61c2caa10e87bc194fc0
pemix09 <pemix09@onet.pl>
2021-06-04 17:50:59 +0200

    FIX FIX FIX

--- Areas/Identity/Pages/Accounts/NewMessage.cshtml.cs
+++ Areas/Identity/Pages/Accounts/NewMessage.cshtml.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SocialNetwork.Models;
 
 namespace SocialNetwork.Areas.Identity.Pages.Accounts
 {
     public class NewMessageModel : PageModel
     {
+        public Message message { get; set; }
         public void OnGet()
         {
         }
