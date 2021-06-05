ab3e8b4cf4b269dfc50592bb990c0f24be5a4b26
pemix09 <pemix09@onet.pl>
2021-06-05 12:02:55 +0200

    Poprawa wyświetlania profilu, dodanie ikonki znajomych do profilui)

--- Areas/Identity/Pages/Accounts/Friends.cshtml.cs
+++ Areas/Identity/Pages/Accounts/Friends.cshtml.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SocialNetwork.Models;
+using SocialNetwork.Data.DAL;
 
 namespace SocialNetwork.Areas.Identity.Pages.Accounts
 {
     public class FriendsModel : PageModel
     {
         public List<Friend> friends = new List<Friend>();
+        public LocalDb db = new LocalDb();
         public void OnGet(string id)
         {
         }
