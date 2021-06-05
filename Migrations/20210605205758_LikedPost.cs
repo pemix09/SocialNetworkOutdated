0497841c895dae0bd33dc4949084d2b053baa13d
pemix09 <pemix09@onet.pl>
2021-06-05 23:00:23 +0200

    Migracja, nowa tabela na polajkowane posty

---
+++ Migrations/20210605205758_LikedPost.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialNetwork.Migrations
+{
+    public partial class LikedPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                schema: "SocialNetwork",
+                table: "Post",
+                type: "nvarchar(45)",
+                maxLength: 45,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                schema: "SocialNetwork",
+                table: "Post",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(45)",
+                oldMaxLength: 45);
+        }
+    }
+}
---
+++ Models/LikedPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models
+{
+    public class LikedPost
+    {
+        [Key]
+        public int ID { get; set; }
+        public string userID { get; set; }
+        public int postID { get; set; }
+    }
+}
